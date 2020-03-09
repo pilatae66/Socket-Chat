7746111f18fa5d5f16b4e33179eec23d73e664e5
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2020-03-09 14:31:43 +0800

    Inital Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require('express')
+let socket = require('socket.io')
+let app = express()
+let server = app.listen(3001, () => {
+    console.log('listening to port 3001')
+})
+
+app.use(express.static('public'))
+
+let io = socket(server)
+io.on('connection', (socket) => {
+    console.log(`connected to port 3001 with socket id of ${socket.id}`);
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+
+    socket.on('not-typing', () => {
+        socket.broadcast.emit('not-typing')
+    })
+
+    socket.on('user-login', (data) => {
+        socket.broadcast.emit('user-login', data)
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,52 @@
+let socket = io.connect('http://localhost:3001')
+
+let username = $('#username'),
+    message = $('#message'),
+    btn = $('#send'),
+    output = $('#output'),
+    feedback = $('#feedback')
+btn.click(() => {
+    socket.emit('chat', {
+        message: message.val(),
+        username: username.val()
+    })
+    message.val('')
+    message.focus()
+})
+
+let timer = null
+message.keydown(() => {
+    clearTimeout(timer)
+    socket.emit('typing', username.val())
+    timer = setTimeout(() => {
+        socket.emit('not-typing')
+    }, 1000);
+})
+
+username.keydown(() => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+        socket.emit('user-login', username.val())
+    }, 1500);
+})
+
+socket.on('chat', (data) => {
+    let htmlString = `<p><strong>${data.username}: </strong>${data.message}</p>`
+    console.log(htmlString)
+    output.append(htmlString)
+    feedback.html('')
+})
+
+socket.on('typing', (data) => {
+    let htmlString = `<p><em>${data} is typing...</em></p>`
+    feedback.html(htmlString)
+})
+
+socket.on('not-typing', () => {
+    feedback.html('')
+})
+
+socket.on('user-login', (data) => {
+    let htmlString = `<p><em>${data} is online.</em></p>`
+    output.append(htmlString)
+})
